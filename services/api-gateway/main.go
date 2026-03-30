815f932737ed2d05297d9f7af53d9d8dc9af902e
inodinwetrust10 <adi4gbsingh@gmail.com>
2026-03-30 09:18:08 +0530

    added graceful shutdowns

--- services/api-gateway/main.go
+++ services/api-gateway/main.go
@@ -2,8 +2,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
@@ -18,7 +23,23 @@ func main() {
 
 	mux.HandleFunc("POST /trip/preview", handleTripPreview)
 
-	if err := server.ListenAndServe(); err != nil {
-		log.Fatal("error starting the server")
+	done := make(chan os.Signal, 1)
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatal("error starting the server")
+		}
+	}()
+
+	signal.Notify(done, os.Interrupt, syscall.SIGTERM)
+
+	<-done
+	log.Println("shutting down the server....")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		log.Printf("graceful shutdown failed: %v", err)
+		if err := server.Close(); err != nil {
+			log.Printf("forced shutdown failed: %v", err)
+		}
 	}
 }
--- services/trip-service/cmd/main.go
+++ services/trip-service/cmd/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	h "ride-sharing/services/trip-service/internal/infrastructure/http"
 	"ride-sharing/services/trip-service/internal/infrastructure/repository"
@@ -20,7 +25,23 @@ func main() {
 		Handler: mux,
 		Addr:    ":8083",
 	}
-	if err := server.ListenAndServe(); err != nil {
-		log.Printf("HTTP server error: %v", err)
+	done := make(chan os.Signal, 1)
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatal("error starting the server")
+		}
+	}()
+
+	signal.Notify(done, os.Interrupt, syscall.SIGTERM)
+
+	<-done
+	log.Println("shutting down the server....")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		log.Printf("graceful shutdown failed: %v", err)
+		if err := server.Close(); err != nil {
+			log.Printf("forced shutdown failed: %v", err)
+		}
 	}
 }
